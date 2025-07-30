6506f05f04d61523fdb4136d41a8d491eb38893d
sumit saraswat <dimmunokha@gmail.com>
2025-07-30 10:36:07 +0530

    modified server.js and controllers

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ const express = require("express");
 const cors  = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
-const incomeRoutes = require("./routes/incomeRoutes");
+const authRoutes = require("./routes/authRoutes.js");
+const incomeRoutes = require("./routes/incomeRoutes.js");
+const expenseRoutes = require("./routes/expenseRoutes.js");
+const dashboardRoutes = require("./routes/dashboardRoutes.js");
+
 
 const app = express();
 
@@ -24,7 +27,11 @@ connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 
-// app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/income", incomeRoutes);
+
+app.use("/api/v1/expense", expenseRoutes);
+
+app.use("/api/v1/dashboard", dashboardRoutes);
 
 
 app.use("/uploads", express.static(path.join(__dirname,"uploads")));
